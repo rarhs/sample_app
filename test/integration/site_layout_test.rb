780d3f4260243b5c3636782999618b62ecfc74be
Abdul Monayam <rmonayam@gmail.com>
2015-08-31 06:16:47 +0000

    solves exercise 9.2 partially

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
+
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+    
+  def setup
+    @admin = users(:michael)
+    @non_admin = users(:archer)
+  end
+  
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,6 +15,30 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-      
   end
+    
+  test "layout links as admin" do
+    log_in_as(@admin)
+    get user_path(@admin)
+    assert_template 'users/show'
+    get users_path
+    assert_template 'users/index'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+  
+  test "layout links as non-admin" do
+    log_in_as(@non_admin)
+    get user_path(@non_admin)
+    assert_template 'users/show'
+    get users_path
+    assert_template 'users/index'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+    
 end
